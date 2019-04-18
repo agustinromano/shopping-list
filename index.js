c4d9dce650c57de26a201dba7196ffdc7b90f7c9
agustinromano <48917174+agustinromano@users.noreply.github.com>
2019-04-18 19:34:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ $(document).ready(function(){
 
     $('#js-shopping-list-form').on('submit', function(event){
         let textInput = $('#shopping-list-entry').val();
-        $('.shopping-list').append('<li>' + textInput + '</li>');
+        $('.shopping-list').append(`<li> <span class="shopping-item">${textInput}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div> </li>`);
 
         event.preventDefault();
     });
